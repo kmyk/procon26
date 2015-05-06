bb77f0c5c53044984cb8a4a5f871126833ae13b2
Kimiyuki Ohnaka <kimiyuki95@gmail.com>
2015-05-06 15:28:41 +0900

    refactoring

--- exact.cpp
+++ exact.cpp
@@ -2,325 +2,116 @@
 #include <vector>
 #include <algorithm>
 #include <cassert>
-typedef long long ll;
-#include <boost/range/irange.hpp>
-namespace boost {
-    template<class Integer>
-    iterator_range< range_detail::integer_iterator<Integer> >
-    irange(Integer last) {
-        return irange(0, last);
-    }
-}
+#include "procon26.hpp"
 using namespace std;
 using namespace boost;
 
-constexpr int board_size = 32;
-constexpr int block_size = 8;
-enum flip_t { H, T };
-enum rot_t { R0, R90, R180, R270 };
-struct board_t {
-    bool cell[board_size][board_size];
-    int x, y, w, h;
-    const bool[board_size] (& operator [] (size_t i) const) { return cell[i]; }
-    bool[board_size]       (& operator [] (size_t i))       { return cell[i]; }
-};
-struct block_t {
-    bool cell[block_size][block_size];
-    int x, y, w, h;
-    int area;
-    const bool[block_size] (& operator [] (size_t i) const) { return cell[i]; }
-    bool[block_size]       (& operator [] (size_t i))       { return cell[i]; }
-};
-struct input_t {
-    board_t board;
-    vector<block_t> blocks;
-};
-struct placement_t {
-    bool used;
-    int x, y;
-    flip_t f;
-    rot_t r;
-};
-struct output_t {
-    vector<placement_t> ps;
-};
-struct point_t { int x, y; };
-
-ostream & operator << (ostream & output, flip_t a) { return output << (a == H ? 'H' : 'T'); }
-ostream & operator << (ostream & output, rot_t a)  { return output << (a == R0 ? 0 : a == R90 ? 90 : a == R180 ? 180 : 270); }
-ostream & operator << (ostream & output, placement_t a)  {
-    if (a.used) output << a.x << " " << a.y << " " << a.f << " " << a.r;
-    return output;
-}
-ostream & operator << (ostream & output, output_t const & a)  {
-    for (auto p : a.ps) output << p << endl;
-    return output;
-}
-
-istream & operator >> (istream & input, board_t & a)  {
-    a.x = a.y = 0;
-    a.w = a.h = board_size;
-    for (int y : irange(a.h)) {
-        for (int x : irange(a.w)) {
-            char c; input >> c;
-            a[y][x] = c == '0';
-        }
-    }
-    return input;
-}
-istream & operator >> (istream & input, block_t & a)  {
-    a.x = a.y = 0;
-    a.w = a.h = block_size;
-    a.area = 0;
-    for (int y : irange(a.h)) {
-        for (int x : irange(a.w)) {
-            char c; input >> c;
-            a[y][x] = c == '1';
-            if (a[y][x]) a.area += 1;
-        }
-    }
-    // > 石は 1 個以上かつ，16 個以下のブロックにより構成され，
-    assert (1 <= a.area and a.area <= 16);
-    return input;
-}
-istream & operator >> (istream & input, input_t & a)  {
-    input >> a.board;
-    int n; input >> n;
-    a.blocks.resize(n);
-    for (int i : irange(n)) input >> a.blocks[i];
-    return input;
-}
-
-template <class T, class F, class G>
-void shrink_impl(T & a, int n, F pred, G update) {
-    while (true) {
-        for (int i : irange(n)) {
-            if (pred(i)) return; // if something found, exit
-        }
-        update(); // shrink
-    }
-}
-template <class T>
-T shrink(T a) {
-    shrink_impl(a, a.h, [&](int dy){ return a[a.y+dy   ][a.x      ]; }, [&](){ a.x += 1; a.w -= 1; });
-    shrink_impl(a, a.w, [&](int dx){ return a[a.y      ][a.x+dx   ]; }, [&](){ a.y += 1; a.h -= 1; });
-    shrink_impl(a, a.h, [&](int dy){ return a[a.y+dy   ][a.x+a.w-1]; }, [&](){           a.w -= 1; });
-    shrink_impl(a, a.w, [&](int dx){ return a[a.y+a.h-1][a.x+dx   ]; }, [&](){           a.h -= 1; });
-    return a;
-}
-
-int block_height(block_t const & block, placement_t const & p) {
-    return (p.r == R0 or p.r == R180) ? block.h : block.w;
-}
-int block_width(block_t const & block, placement_t const & p) {
-    return (p.r == R0 or p.r == R180) ? block.w: block.h;
-}
-point_t block_local(block_t const & block, placement_t const & p, int y, int x) {
-    int a = block.y;
-    int b = block.x;
-    int h = block.h;
-    int w = block.w;
-    // > 石操作の順番として，はじめに石の裏表を決め，そのあとに回転させる。
-    switch (p.f) {
-        case H:
-            switch (p.r) {
-                case R0:   return { b+  x,   a+  y   };
-                case R90:  return { b+  y,   a+h-x-1 };
-                case R180: return { b+w-x-1, a+h-y-1 };
-                case R270: return { b+w-y-1, a+  x   };
-            }
-        case T:
-            switch (p.r) {
-                case R0:   return { b+w-x-1, a+  y   };
-                case R90:  return { b+w-y-1, a+h-x-1 };
-                case R180: return { b+  x,   a+h-y-1 };
-                case R270: return { b+  y,   a+  x   };
-            }
-    }
-}
-bool block_at(block_t const & block, placement_t const & p, int y, int x) {
-    point_t q = block_local(block,p,y,x);
-    return block[q.y][q.x];
-}
-point_t block_offset(block_t const & block, placement_t const & p) {
-    int a = block.y;
-    int b = block.x;
-    int h = block.h;
-    int w = block.w;
-    int s = block_size;
-    switch (p.f) {
-        case H:
-            switch (p.r) {
-                case R0:   return {   b,     a   };
-                case R90:  return { s-a-h,   b   };
-                case R180: return { s-b-w, s-a-h };
-                case R270: return {   a,   s-b-w };
-            }
-        case T:
-            switch (p.r) {
-                case R0:   return { s-b-w,   a   };
-                case R90:  return { s-a-h, s-b-w };
-                case R180: return {   b,   s-a-h };
-                case R270: return {   a,     b   };
-            }
-    }
-}
-point_t block_world(block_t const & block, placement_t const & p, int y, int x) {
-    point_t q = block_offset(block,p);
-    return { x + p.x + q.x, y + p.y + q.y };
-}
-bool is_block_puttable(block_t const & block, placement_t const & p, bool (& used)[board_size][board_size], board_t const & board, bool connected) {
-    for (int dy : irange(block_height(block,p))) {
-        for (int dx : irange(block_width(block,p))) {
-            if (block_at(block,p,dy,dx)) {
-                int y = block_world(block,p,dy,dx).y;
-                int x = block_world(block,p,dy,dx).x;
-                if (not (0 <= y and y < board_size)) return false;
-                if (not (0 <= x and x < board_size)) return false;
-                if (used[y][x] or not board[y][x]) return false;
-                if (not connected) {
-                    connected = (y+1 < board_size and used[y+1][x])
-                        or (0 <= y-1 and used[y-1][x])
-                        or (x+1 < board_size and used[y][x+1])
-                        or (0 <= x-1 and used[y][x-1]);
-                }
-            }
-        }
-    }
-    return connected;
-}
-void block_put(block_t const & block, placement_t const & p, bool (& used)[board_size][board_size], bool z) {
-    for (int dy : irange(block_height(block,p))) {
-        for (int dx : irange(block_width(block,p))) {
-            if (block_at(block,p,dy,dx)) {
-                int y = block_world(block,p,dy,dx).y;
-                int x = block_world(block,p,dy,dx).x;
-                used[y][x] = z;
-            }
-        }
-    }
-}
-
-// TODO: should I make a class to pack the state?
-void dfs(vector<placement_t> & acc, int score, bool (& used)[board_size][board_size], board_t const & board, vector<block_t> const & blocks, vector<placement_t> & result, int & highscore) {
-    int l = acc.size();
-    if (l == blocks.size()) {
-        // when you have put all blocks
-        if (highscore < score) {
-            highscore = score;
-            result = acc;
-#ifndef NDEBUG
-            cerr << "highscore: " << highscore << endl;
-#endif
-        }
-        return;
-    }
-    block_t const & block = blocks[l];
-    for (int s : irange(8)) {
-        placement_t p;
-        p.used = true;
-        p.f = (flip_t)(s / 4); // R0, R90, R180, R270
-        p.r =  (rot_t)(s % 4); // H or T
-        // is this range correct?
-        for (int y : irange(board.y-block_height(block,p), board.y+board.h)) {
-            p.y = y;
-            for (int x : irange(board.x-block_width(block,p), board.x+board.w)) {
-                p.x = x;
-                if (is_block_puttable(block, p, used, board, score == 0)) {
-                    block_put(block, p, used, true);
-                    acc.push_back(p);
-                    dfs(acc, score + block.area, used, board, blocks, result, highscore);
-                    acc.pop_back();
-                    block_put(block, p, used, false);
-                }
-            }
-        }
-    }
-    acc.push_back({ false });
-    dfs(acc, score, used, board, blocks, result, highscore);
-    acc.pop_back();
-}
-
-output_t solve(board_t board, vector<block_t> blocks) {
-    board = shrink(board);
-    for (auto & block : blocks) block = shrink(block);
-#ifndef NDEBUG
-    cerr << "board size: (" << board.w << ", " << board.h << ")" << endl;
-    for (auto & block : blocks) cerr << "block size: (" << block.w << ", " << block.h << ")" << endl;
-#endif
-    vector<placement_t> acc;
-    bool used[board_size][board_size] = {};
+class solver {
+    board brd;
+    vector<block> blks;
     vector<placement_t> result;
-    int highscore = -1;
-    dfs(acc, 0, used, board, blocks, result, highscore);
-    assert (result.size() == blocks.size());
-    return { result };
-}
+    int highscore;
+public:
+    solver() = default;
 
-void test();
+public:
+    output_t operator () (input_t const & a) {
+        brd = board(a.board);
+        blks.resize(a.blocks.size()); for (int i : irange(blks.size())) blks[i] = block(a.blocks[i]);
+#ifndef NDEBUG
+        cerr << "board size: " << brd.size() << endl;
+        for (auto & blk : blks) cerr << "block size: " << blk.size(H,R0) << endl;
+#endif
+        result.clear();
+        highscore = -1;
+        vector<placement_t> acc;
+        bool used[board_size][board_size] = {};
+        dfs(acc, 0, used);
+#ifndef NEBUG
+        assert (result.size() == a.blocks.size());
+        assert (acc.size() == 0);
+        for (int y : irange(board_size)) {
+            for (int x : irange(board_size)) {
+                assert (not used[y][x]);
+            }
+        }
+#endif
+        return { result };
+    }
+
+private:
+    bool is_puttable(block const & blk, placement_t const & p, bool const (& used)[board_size][board_size], bool is_first) {
+        bool connected = false;
+        for (int dy : irange(blk.h(p))) {
+            for (int dx : irange(blk.w(p))) {
+                if (blk.at(p.f,p.r,{dx,dy})) {
+                    point_t q = blk.world(p,{dx,dy});
+                    int x = q.x;
+                    int y = q.y;
+                    if (not (0 <= y and y < board_size)) return false;
+                    if (not (0 <= x and x < board_size)) return false;
+                    if (used[y][x] or not brd.cell()[y][x]) return false;
+                    if (not connected) {
+                        connected = (y+1 < board_size and used[y+1][x])
+                            or (0 <= y-1 and used[y-1][x])
+                            or (x+1 < board_size and used[y][x+1])
+                            or (0 <= x-1 and used[y][x-1]);
+                    }
+                }
+            }
+        }
+        if (not is_first and not connected) return false;
+        return true;
+    }
+    void put(block const & blk, placement_t const & p, bool v, bool (& used)[board_size][board_size]) {
+        for (int dy : irange(blk.h(p))) {
+            for (int dx : irange(blk.w(p))) {
+                if (blk.at(p.f,p.r,{dx,dy})) {
+                    point_t q = blk.world(p,{dx,dy});
+                    used[q.y][q.x] = v;
+                }
+            }
+        }
+    }
+
+    void dfs(vector<placement_t> & acc, int score, bool (& used)[board_size][board_size]) {
+        int l = acc.size();
+        if (l == blks.size()) {
+            if (highscore < score) {
+                highscore = score;
+                result = acc;
+#ifndef NDEBUG
+                cerr << "highscore: " << highscore << endl;
+#endif
+            }
+            return;
+        }
+        block const & blk = blks[l];
+        for (flip_t f : { H, T }) {
+            for (rot_t r : { R0, R90, R180, R270 }) {
+                for (int y : irange(brd.y()-blk.h(f,r), brd.y()+brd.h())) {
+                    for (int x : irange(brd.x()-blk.w(f,r), brd.x()+brd.w())) {
+                        placement_t p = { true, { x, y }, f, r };
+                        if (is_puttable(blk, p, used, score == 0)) {
+                            put(blk, p, true, used);
+                            acc.push_back(p);
+                            dfs(acc, score+blk.area(), used);
+                            acc.pop_back();
+                            put(blk, p, false, used);
+                        }
+                    }
+                }
+            }
+        }
+        acc.push_back({ false });
+        dfs(acc, score, used);
+        acc.pop_back();
+    }
+};
+
 int main() {
-    test();
     ios_base::sync_with_stdio(false);
     input_t a; cin >> a;
-    cout << solve(a.board, a.blocks);
+    cout << solver()(a);
     return 0;
-}
-
-void test() {
-    {
-        block_t a = {};
-        a[3][1] = true; //  12
-        a[3][2] = true; // 3##
-        a[4][2] = true; // 4 #
-        a.y = 3; a.x = 1;
-        a.h = 2; a.w = 2;
-        a.area = 3;
-        placement_t p = { true };
-        p.x = 0;
-        p.y = 0;
-        p.f = H;
-        p.r = R0;
-        assert (block_at(a,p,0,0) == true);
-        assert (block_at(a,p,0,1) == true);
-        assert (block_at(a,p,1,0) == false);
-        assert (block_at(a,p,1,1) == true);
-        p.f = T;
-        assert (block_at(a,p,0,0) == true);
-        assert (block_at(a,p,0,1) == true);
-        assert (block_at(a,p,1,0) == true);
-        assert (block_at(a,p,1,1) == false);
-        p.r = R90;
-        assert (block_at(a,p,0,0) == true);
-        assert (block_at(a,p,0,1) == true);
-        assert (block_at(a,p,1,0) == false);
-        assert (block_at(a,p,1,1) == true);
-    }
-    {
-        block_t a = {};
-        a[5][4] = true; //  345
-        a[6][3] = true; // 5 #
-        a[6][4] = true; // 6###
-        a[6][5] = true;
-        a.y = 5; a.x = 3;
-        a.h = 2; a.w = 3;
-        a.area = 4;
-        placement_t p = { true };
-        p.x = 0;
-        p.y = 0;
-        p.f = H;
-        p.r = R0;
-        assert (block_at(a,p,0,0) == false);
-        assert (block_at(a,p,0,1) == true);
-        assert (block_at(a,p,0,2) == false);
-        assert (block_at(a,p,1,0) == true);
-        assert (block_at(a,p,1,1) == true);
-        assert (block_at(a,p,1,2) == true);
-        p.r = R90;
-        assert (block_at(a,p,0,0) == true);
-        assert (block_at(a,p,0,1) == false);
-        assert (block_at(a,p,1,0) == true);
-        assert (block_at(a,p,1,1) == true);
-        assert (block_at(a,p,2,0) == true);
-        assert (block_at(a,p,2,1) == false);
-    }
 }
---
+++ procon26.hpp
@@ -0,0 +1,239 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+// #include <boost/geometry.hpp>
+#include <boost/range/irange.hpp>
+namespace boost {
+    template<class Integer>
+    iterator_range< range_detail::integer_iterator<Integer> >
+    irange(Integer last) {
+        return boost::irange(static_cast<Integer>(0), last);
+    }
+}
+
+constexpr int board_size = 32;
+constexpr int block_size = 8;
+
+struct board_t {
+    // 石を置けるか
+    bool a[board_size][board_size]; // '0' => true
+};
+struct block_t {
+    bool a[block_size][block_size]; // '1' => true
+};
+struct input_t {
+    board_t board;
+    std::vector<block_t> blocks;
+};
+
+inline std::istream & operator >> (std::istream & input, board_t & a)  {
+    using namespace boost;
+    for (int y : irange(board_size)) {
+        for (int x : irange(board_size)) {
+            char c; input >> c;
+            a.a[y][x] = c == '0';
+        }
+    }
+    return input;
+}
+inline std::istream & operator >> (std::istream & input, block_t & a)  {
+    using namespace boost;
+    for (int y : irange(block_size)) {
+        for (int x : irange(block_size)) {
+            char c; input >> c;
+            a.a[y][x] = c == '1';
+        }
+    }
+    return input;
+}
+inline std::istream & operator >> (std::istream & input, input_t & a)  {
+    using namespace boost;
+    input >> a.board;
+    int n; input >> n;
+    a.blocks.resize(n);
+    for (int i : irange(n)) input >> a.blocks[i];
+    return input;
+}
+
+enum flip_t { H, T };
+enum rot_t { R0, R90, R180, R270 };
+struct point_t { int x, y; };
+struct placement_t {
+    bool used;
+    point_t p;
+    flip_t f;
+    rot_t r;
+};
+struct output_t {
+    std::vector<placement_t> ps;
+};
+inline rot_t rot90(rot_t r) {
+    return static_cast<rot_t>((r + 1) % 4);
+}
+
+inline std::ostream & operator << (std::ostream & output, flip_t a) { return output << (a == H ? 'H' : 'T'); }
+inline std::ostream & operator << (std::ostream & output, rot_t a)  { return output << (a == R0 ? 0 : a == R90 ? 90 : a == R180 ? 180 : 270); }
+inline std::ostream & operator << (std::ostream & output, placement_t a)  {
+    if (a.used) output << a.p.x << " " << a.p.y << " " << a.f << " " << a.r;
+    return output;
+}
+inline std::ostream & operator << (std::ostream & output, output_t const & a)  {
+    for (auto p : a.ps) output << p << std::endl;
+    return output;
+}
+inline std::ostream & operator << (std::ostream & output, point_t const & a) { return output << "(" << a.x << ", " << a.y << ")"; }
+
+template <int N>
+void copy_cell(bool const (& src)[N][N], bool (& dst)[N][N]) {
+    using namespace boost;
+    for (int y : irange(N)) {
+        for (int x : irange(N)) {
+            dst[y][x] = src[y][x];
+        }
+    }
+}
+template <int N>
+void copy_cell_rot90(bool const (& src)[N][N], bool (& dst)[N][N]) {
+    using namespace boost;
+    for (int y : irange(N)) {
+        for (int x : irange(N)) {
+            // 8×8 のマス全体を時計回りに 90 度，180 度，270 度回転させることができる。
+            dst[x][N-1-y] = src[y][x];
+        }
+    }
+}
+template <int N>
+void copy_cell_flip(bool const (& src)[N][N], bool (& dst)[N][N]) {
+    using namespace boost;
+    for (int y : irange(N)) {
+        for (int x : irange(N)) {
+            // > 裏返した後は，与えられたときの配置と左右に反転しているものとする。
+            dst[y][N-x-1] = src[y][x];
+        }
+    }
+}
+template <class F, class G>
+void shrink_cell_helper(int n, F pred, G update) {
+    using namespace boost;
+    while (true) {
+        for (int i : irange(n)) {
+            if (pred(i)) return; // if something found, exit
+        }
+        update(); // shrink
+    }
+}
+template <int N>
+void shrink_cell(bool (& a)[N][N], point_t & offset, point_t & size) {
+    int & x = offset.x;
+    int & y = offset.y;
+    int & w = size.x;
+    int & h = size.y;
+    shrink_cell_helper(h, [&](int dy){ return a[y+dy ][x    ]; }, [&](){ x += 1; w -= 1; });
+    shrink_cell_helper(w, [&](int dx){ return a[y    ][x+dx ]; }, [&](){ y += 1; h -= 1; });
+    shrink_cell_helper(h, [&](int dy){ return a[y+dy ][x+w-1]; }, [&](){         w -= 1; });
+    shrink_cell_helper(w, [&](int dx){ return a[y+h-1][x+dx ]; }, [&](){         h -= 1; });
+}
+template <int N>
+int area_cell(bool const (& a)[N][N]) {
+    using namespace boost;
+    int n = 0;
+    for (int y : irange(N)) {
+        for (int x : irange(N)) {
+            if (a[y][x]) {
+                n += 1;
+            }
+        }
+    }
+    return n;
+}
+
+class board {
+public:
+    static constexpr int N = board_size;
+private:
+    bool m_cell[N][N]; // m_cells?
+    point_t m_offset;
+    point_t m_size;
+    int m_area;
+public:
+    board() = default;
+    board(board_t const & a) {
+        m_offset = { 0, 0 };
+        m_size = { N, N };
+        copy_cell(a.a, m_cell);
+        m_area = area_cell(a.a);
+    }
+
+public:
+    bool const (& cell() const) [N][N] { return m_cell; }
+    point_t offset() const { return m_offset; }
+    point_t size() const { return m_size; }
+    int area() const { return m_area; }
+    int x() const { return m_offset.x; }
+    int y() const { return m_offset.y; }
+    int w() const { return m_size.x; }
+    int h() const { return m_size.y; }
+};
+
+class block {
+public:
+    static constexpr int N = block_size;
+private:
+    bool m_cell[2][4][N][N]; // m_cells?
+    point_t m_offset[2][4];
+    point_t m_size[2][4];
+    int m_area;
+public:
+    block() = default;
+    block(block_t const & a) {
+        for (flip_t f : { H, T }) {
+            for (rot_t r : { R0, R90, R180, R270 }) {
+                m_offset[f][r] = { 0, 0 };
+                m_size[f][r] = { N, N };
+            }
+        }
+        // > 石は 1 個以上かつ，16 個以下のブロックにより構成され，
+        m_area = area_cell(a.a);
+        assert (1 <= m_area and m_area <= 16);
+        // > 石操作の順番として，はじめに石の裏表を決め，そのあとに回転させる。
+        copy_cell(a.a, m_cell[H][R0]);
+        copy_cell_flip(m_cell[H][R0], m_cell[T][R0]);
+        for (flip_t f : { H, T }) {
+            for (rot_t r : { R0, R90, R180 }) {
+                copy_cell_rot90(m_cell[f][r], m_cell[f][rot90(r)]);
+            }
+        }
+        for (flip_t f : { H, T }) {
+            for (rot_t r : { R0, R90, R180, R270 }) {
+                shrink_cell(m_cell[f][r], m_offset[f][r], m_size[f][r]);
+            }
+        }
+    }
+
+public:
+    bool const (& cell(flip_t f, rot_t r) const) [N][N] { return m_cell[f][r]; }
+    point_t offset(flip_t f, rot_t r) const { return m_offset[f][r]; }
+    point_t size(flip_t f, rot_t r) const { return m_size[f][r]; }
+    point_t offset(placement_t const & p) const { assert (p.used); return offset(p.f,p.r); }
+    point_t size(placement_t const & p) const { assert (p.used); return size(p.f,p.r); }
+    int area() const { return m_area; }
+    int w(flip_t f, rot_t r) const { return size(f,r).x; }
+    int h(flip_t f, rot_t r) const { return size(f,r).y; }
+    int w(placement_t const & p) const { return w(p.f,p.r); }
+    int h(placement_t const & p) const { return h(p.f,p.r); }
+    point_t local(flip_t f, rot_t r, point_t const & q) const {
+        int ay = offset(f,r).y + q.y;
+        int ax = offset(f,r).x + q.x;
+        return { ax, ay };
+    }
+    bool at(flip_t f, rot_t r, point_t p) const {
+        p = local(f,r,p);
+        return m_cell[f][r][p.y][p.x];
+    }
+    point_t world(placement_t const & p, point_t const & q) const {
+        assert (p.used);
+        int ay = p.p.y + offset(p.f,p.r).y + q.y;
+        int ax = p.p.x + offset(p.f,p.r).x + q.x;
+        return { ax, ay };
+    }
+};
