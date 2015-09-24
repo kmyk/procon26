c7f0de4abd52c83c649f036bdd44e1aa50b93d5d
Kimiyuki Onaka <kimiyuki95@gmail.com>
2015-09-24 15:16:38 +0900

    *バグ*

--- forward.cpp
+++ forward.cpp
@@ -6,9 +6,20 @@
 #include "procon26.hpp"
 using namespace std;
 
+/**
+ * 100000001 100000001
+ * 101110101 100000001
+ * 101010101 111111111
+ * 111111111 111111111
+ *
+ * heuristikに考えて右の方がいいよね、境界線が滑らかで、連結で、
+ * そこで境界の長さを考えてみたよ
+ */
+
 struct photon_t {
     board brd;
     int score;
+    int circumference;
     vector<placement_t> plc;
     int bix; // current block
     vector<bool> used; // are blocks used
@@ -16,12 +27,16 @@ struct photon_t {
     point_t rp; // bounding box
 };
 
-// larger iff better
-bool operator < (photon_t const & a, photon_t const & b) {
-    return - a.score < - b.score;
+double evaluate(photon_t const & a) {
+    return - a.circumference - a.score * 20;
 }
 
-constexpr int beam_width = 64;
+// larger iff better
+bool operator < (photon_t const & a, photon_t const & b) {
+    return evaluate(a) < evaluate(b);
+}
+
+constexpr int beam_width = 512;
 
 class forward_solver {
     board brd; // immutable
@@ -39,6 +54,7 @@ public:
             photon_t pho;
             pho.brd = brd;
             pho.score = brd.area();
+            pho.circumference = 0; // 相対的なもののみ気にする
             pho.plc.resize(n, { false });
             pho.bix = 0;
             pho.used.resize(n);
@@ -46,7 +62,9 @@ public:
             pho.rp = brd.offset() + brd.size();
             beam.push_back(pho);
         }
+int nthbeam = 0;
         while (not beam.empty()) {
+cerr << "beam " << (nthbeam ++) << " : " << beam.size() << endl;
             vector<photon_t> next;
             for (auto const & pho : beam) {
                 if (pho.score < highscore) {
@@ -64,22 +82,30 @@ public:
                     npho.used[bix] = true;
                     npho.score -= blk.area();
                     placement_t p = initial_placement(blk, pho.lp);
-                    do if (is_puttable(brd, blk, p)) {
-                        if (is_puttable(pho.brd, blk, p)) {
-                            npho.lp = p.p + blk.offset(p); // 新たなbounding box
-                            npho.rp = p.p + blk.offset(p) + blk.size(p);
-                            if (not brd.is_new()) { // 古いやつと合成
-                                npho.lp = pwmin(npho.lp, pho.lp);
-                                npho.rp = pwmax(npho.rp, pho.rp);
-                            }
-                            put_stone(npho.brd, blk, p, 2+bix);
-                            npho.brd.update();
-                            npho.plc[bix] = p;
-                            next.push_back(npho);
-                            put_stone(npho.brd, blk, p, 0);
-                            npho.brd.update();
-                            npho.plc[bix] = { false };
+                    do if (is_puttable(pho.brd, blk, p)) {
+                        npho.lp = p.p + blk.offset(p); // 新たなbounding box
+                        npho.rp = p.p + blk.offset(p) + blk.size(p);
+                        if (not brd.is_new()) { // 古いやつと合成
+                            npho.lp = pwmin(npho.lp, pho.lp);
+                            npho.rp = pwmax(npho.rp, pho.rp);
                         }
+                        for (auto q : blk.stones(p)) {
+                            repeat (i,4) {
+                                auto r = q + dp[i];
+                                npho.circumference +=
+                                    not is_on_board(r) ? 1 :
+                                    brd.at(q) == 0 ? 1 :
+                                    -1;
+                            }
+                            npho.brd.put(q, 2+bix);
+                        }
+                        npho.brd.update();
+                        npho.plc[bix] = p;
+                        next.push_back(npho);
+                        put_stone(npho.brd, blk, p, 0);
+                        npho.brd.update();
+                        npho.plc[bix] = { false };
+                        npho.circumference = pho.circumference;
                     } while (next_placement(p, blk, pho.lp, pho.rp));
                 }
             }
