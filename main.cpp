7e555b26218b4fe7d4b62c898b018f8314be56d4
Kimiyuki Onaka <kimiyuki95@gmail.com>
2015-10-10 02:13:58 +0900

    出力をファイルにも控えるように 残念ながらコンパイル時間はboostされました

--- main.cpp
+++ main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <csignal>
+#include <boost/filesystem.hpp>
 #include "procon26.hpp"
 #include "exact.hpp"
 #include "forward.hpp"
 
 using namespace std;
+
+void export_to_file(output_t const & a) {
+    string path; {
+        int i = 0;
+        do {
+            ostringstream oss;
+            oss << ".log-" << i << ".txt";
+            path = oss.str();
+        } while (boost::filesystem::exists(path));
+    }
+    ofstream fp(path, ios::out);
+    fp << a;
+}
 
 output_t g_provisional_result; // externed
 
@@ -12,6 +28,7 @@ void signal_handler(int param) {
     cerr << "*** signal " << param << " caught ***" << endl;
     cout << g_provisional_result;
     cerr << "***" << endl;
+    export_to_file(g_provisional_result);
     exit(param);
 }
 
@@ -23,8 +40,10 @@ int main() {
     board brd = board(a.board);
     vector<block> blks(n); repeat (i,n) blks[i] = block(a.blocks[i]);
     signal(SIGINT, &signal_handler);
-    cout << (output_t) { SOLVER(brd, blks) };
+    output_t b = { SOLVER(brd, blks) };
     signal(SIGINT, SIG_DFL);
+    cout << b;
+    export_to_file(b);
     return 0;
 }
 #endif
